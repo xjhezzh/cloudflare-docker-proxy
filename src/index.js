8b8871c09b8d87cf22c1fba0035e77c9c60abaf3
xjhezzh <56908264+xjhezzh@users.noreply.github.com>
2025-08-14 16:04:41 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,9 +34,19 @@ function routeByHosts(host) {
 
 async function handleRequest(request) {
   const url = new URL(request.url);
-  if (url.pathname == "/") {
-    return Response.redirect(url.protocol + "//" + url.host + "/v2/", 301);
+  // return docs
+  if (url.pathname === "/") {
+    return new Response(DOCS, {
+      status: 200,
+      headers: {
+        "content-type": "text/html"
+      }
+    });
   }
+  // orgi
+  //if (url.pathname == "/") {
+  //  return Response.redirect(url.protocol + "//" + url.host + "/v2/", 301);
+  //}
   const upstream = routeByHosts(url.hostname);
   if (upstream === "") {
     return new Response(
@@ -48,15 +58,7 @@ async function handleRequest(request) {
       }
     );
   }
-  // return docs
-  if (url.pathname === "/") {
-    return new Response(DOCS, {
-      status: 200,
-      headers: {
-        "content-type": "text/html"
-      }
-    });
-  }
+  
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
   if (url.pathname == "/v2/") {
